cdc10d07254257a68b870efd26721ee6ff65a18d
Bruno Gonçalves <brunogcp28@gmail.com>
2023-09-25 09:00:32 -0400

    Tipos primitivos

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+    String
+    Number
+    Boolean
+    Null
+    Undefined
+*/
+
+
+let name = "Bruno"
+let number = 123
+let usuario = true
+let pessoa = null
+let telefone = undefined
+
+
+
+
